a1e452943477df99bacdf16c64eb04a8c96b88ce
IvanKushchenko <kushhenko-ivan@yandex.ru>
2020-07-01 16:09:49 +0300

    rebuild

--- webpack.config.js
+++ webpack.config.js
@@ -15,99 +15,92 @@ module.exports = {
 		port: 4000
 	},
 	plugins: [
-		// new BrowserSyncPlugin({
-		// 	server: {baseDir: ['public/'] },
-		// 	ui: false,
-		// 	port: 4000,
-		// 	open: false,
-		// 	notify: false,
-		// }),
 		new MiniCssExtractPlugin({
 			filename: 'css/style.css'
 		}),
-		// new HtmlWebpackPlugin({
-		// 	template: './assets/pug/index.pug',
-		// 	minify: false,
-		// 	hash: true
-		// }),
-		// new HtmlWebpackPlugin({
-		// 	filename: 'jobs.html',
-		// 	template: './assets/pug/jobs.pug',
-		// 	minify: false,
-		// 	hash: true
-		// }),
-		// new HtmlWebpackPlugin({
-		// 	filename: 'basket.html',
-		// 	template: './assets/pug/basket.pug',
-		// 	minify: false,
-		// 	hash: true
-		// }),
+		new HtmlWebpackPlugin({
+			template: './assets/pug/index.pug',
+			minify: false,
+			hash: true
+		}),
+		new HtmlWebpackPlugin({
+			filename: 'jobs.html',
+			template: './assets/pug/jobs.pug',
+			minify: false,
+			hash: true
+		}),
+		new HtmlWebpackPlugin({
+			filename: 'basket.html',
+			template: './assets/pug/basket.pug',
+			minify: false,
+			hash: true
+		}),
 		new HtmlWebpackPlugin({
 			filename: 'index-radio.html',
 			template: './assets/pug/index-radio.pug',
 			minify: false,
 			hash: true
 		}),
-		// new HtmlWebpackPlugin({
-		// 	filename: 'brand.html',
-		// 	template: './assets/pug/brand.pug',
-		// 	minify: false,
-		// 	hash: true
-		// }),
-		// new HtmlWebpackPlugin({
-		// 	filename: 'brands.html',
-		// 	template: './assets/pug/brands.pug',
-		// 	minify: false,
-		// 	hash: true
-		// }),
+		new HtmlWebpackPlugin({
+			filename: 'brand.html',
+			template: './assets/pug/brand.pug',
+			minify: false,
+			hash: true
+		}),
+		new HtmlWebpackPlugin({
+			filename: 'brands.html',
+			template: './assets/pug/brands.pug',
+			minify: false,
+			hash: true
+		}),
 		// new HtmlWebpackPlugin({
 		// 	filename: 'kabinet.html',
 		// 	template: './assets/pug/kabinet.pug',
 		// 	minify: false,
 		// 	hash: true
 		// }),
-		// new HtmlWebpackPlugin({
-		// 	filename: 'shares.html',
-		// 	template: './assets/pug/shares.pug',
-		// 	minify: false,
-		// 	hash: true
-		// }),
-		// new HtmlWebpackPlugin({
-		// 	filename: 'share.html',
-		// 	template: './assets/pug/share.pug',
-		// 	minify: false,
-		// 	hash: true
-		// }),
-		// new HtmlWebpackPlugin({
-		// 	filename: 'adjustable-page.html',
-		// 	template: './assets/pug/adjustable-page.pug',
-		// 	minify: false,
-		// 	hash: true
-		// }),
-		// new HtmlWebpackPlugin({
-		// 	filename: 'main.html',
-		// 	template: './assets/pug/main.pug',
-		// 	minify: false,
-		// 	hash: true
-		// }),
-		// new HtmlWebpackPlugin({
-		// 	filename: 'contacts.html',
-		// 	template: './assets/pug/contacts.pug',
-		// 	minify: false,
-		// 	hash: true
-		// }),
-		// new HtmlWebpackPlugin({
-		// 	filename: '404.html',
-		// 	template: './assets/pug/404.pug',
-		// 	minify: false,
-		// 	hash: true
-		// }),
-		// new HtmlWebpackPlugin({
-		// 	filename: 'catalog.html',
-		// 	template: './assets/pug/catalog.pug',
-		// 	minify: false,
-		// 	hash: true
-		// }),
+		new HtmlWebpackPlugin({
+			filename: 'shares.html',
+			template: './assets/pug/shares.pug',
+			minify: false,
+			hash: true
+		}),
+		new HtmlWebpackPlugin({
+			filename: 'share.html',
+			template: './assets/pug/share.pug',
+			minify: false,
+			hash: true
+		}),
+		new HtmlWebpackPlugin({
+			filename: 'adjustable-page.html',
+			template: './assets/pug/adjustable-page.pug',
+			minify: false,
+			hash: true
+		}),
+		new HtmlWebpackPlugin({
+			filename: 'main.html',
+			template: './assets/pug/main.pug',
+			minify: false,
+			hash: true
+		}),
+		new HtmlWebpackPlugin({
+			filename: 'contacts.html',
+			template: './assets/pug/contacts.pug',
+			minify: false,
+			hash: true
+		}),
+		new HtmlWebpackPlugin({
+			filename: '404.html',
+			template: './assets/pug/404.pug',
+			minify: false,
+			hash: true
+		}),
+		new HtmlWebpackPlugin({
+			filename: 'catalog.html',
+			template: './assets/pug/catalog.pug',
+			minify: false,
+			hash: true
+		}),
 		new FileManagerPlugin({
 			onEnd: {
 				copy: [
